44e0d54cd487d3e1c60b2d424d69f08e2308b800
Salma Hamed <salma.7amed@gmail.com>
2016-04-06 23:16:18 +0200

    switched db to mysql

--- djangotask/settings.py
+++ djangotask/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'djangotask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+            'read_default_file': os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)),'db.cnf'))
+        }
     }
 }
 
